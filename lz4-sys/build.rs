eefe032b7f795a0f9a2ea30a3e51115cf5060761
Thomas Orozco <torozco@fb.com>
2019-05-31 20:21:42 +0100

    Allow delegating all linking
    This is necessary when an external build system (e.g. Buck) is taking
    care of linking in liblz4, and we don't want the Rust compiler to emit
    any linking arguments.

--- lz4-sys/build.rs
+++ lz4-sys/build.rs
@@ -2,7 +2,7 @@ extern crate cc;
 
 use std::env;
 
-fn main() {
+fn build_vendored() {
     let mut compiler = cc::Build::new();
     compiler
         .file("liblz4/lib/lz4.c")
@@ -11,13 +11,23 @@ fn main() {
         .file("liblz4/lib/xxhash.c")
         // We always compile the C with optimization, because otherwise it is 20x slower.
         .opt_level(3);
-    match env::var("TARGET").unwrap().as_str()
-    {
-      "i686-pc-windows-gnu" => {
-        compiler
-            .flag("-fno-tree-vectorize");
-      },
-      _ => {}
+    match env::var("TARGET").unwrap().as_str() {
+        "i686-pc-windows-gnu" => {
+            compiler.flag("-fno-tree-vectorize");
+        }
+        _ => {}
     }
     compiler.compile("liblz4.a");
+}
+
+fn main() {
+    let k = "LZ4_SYS_DELEGATE_LINKING";
+
+    println!("cargo:rerun-if-env-changed={}", k);
+
+    if env::var(k).is_ok() {
+        return;
+    }
+
+    build_vendored();
 }
