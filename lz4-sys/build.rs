df9afbb2f1299c1ebfa42e14bfbd16c7bee57e5f
Thomas Orozco <torozc@fb.com>
2019-05-31 20:21:42 +0100

    Allow linking to an external liblz4

--- lz4-sys/build.rs
+++ lz4-sys/build.rs
@@ -2,7 +2,7 @@ extern crate cc;
 
 use std::env;
 
-fn main() {
+fn build_vendored() {
     let mut compiler = cc::Build::new();
     compiler
         .file("liblz4/lib/lz4.c")
@@ -13,11 +13,27 @@ fn main() {
         .opt_level(3);
     match env::var("TARGET").unwrap().as_str()
     {
-      "i686-pc-windows-gnu" => {
-        compiler
-            .flag("-fno-tree-vectorize");
-      },
-      _ => {}
+        "i686-pc-windows-gnu" => {
+            compiler
+                .flag("-fno-tree-vectorize");
+        },
+        _ => {}
     }
     compiler.compile("liblz4.a");
+}
+
+fn use_lib(dir: String) {
+    println!("cargo:warning=using liblz4 from: {}", dir);
+    println!("cargo:rustc-link-search={}/lib", dir);
+    println!("cargo:rustc-link-lib=lz4");
+    println!("cargo:include={}/include", dir);
+}
+
+fn main() {
+    println!("cargo:rerun-if-env-changed=LIBLZ4_DIR");
+
+    match env::var("LIBLZ4_DIR") {
+        Ok(dir) => use_lib(dir),
+        Err(_) => build_vendored(),
+    }
 }
